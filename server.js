1ac9016baf4b0cf70b670763d229cc0085b80c9c
Ka-iPy <kaighost65@gmail.com>
2026-02-06 17:04:39 +0100

    Committing

--- server.js
+++ server.js
@@ -1,93 +1,102 @@
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
+const { MongoClient } = require('mongodb')
 require('dotenv').config()
 
+const PORT = process.env.PORT || 2121
+const dbConnectionStr = process.env.DB_STRING || "mongodb+srv://kaighost65_db_user:XTMPdkziXrOhPkma@starwars.kpugoe8.mongodb.net/?appName=starwars"
+const dbName = 'starwars'
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
+// Initialize Database Connection
+async function connectDB() {
+    try {
+        const client = await MongoClient.connect(dbConnectionStr)
         db = client.db(dbName)
-    })
-    
+        console.log(`Connected to ${dbName} Database`)
+        
+        // Start server only after DB connection
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+    } catch (err) {
+        console.error('Database connection failed:', err.message)
+        process.exit(1)
+    }
+}
+
+connectDB()
+
+// Middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+// Routes
+app.get('/', async (req, res) => {
+    try {
+        const todoItems = await db.collection('starwars').find().toArray()
+        const itemsLeft = await db.collection('starwars').countDocuments({ completed: false })
+        res.render('index.ejs', { items: todoItems, left: itemsLeft })
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Internal Server Error')
+    }
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
-
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
+app.post('/addTodo', async (req, res) => {
+    try {
+        await db.collection('starwars').insertOne({
+            thing: req.body.todoItem,
             completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
+        })
+        console.log('Todo added!')
+        res.redirect('/')
+    } catch (err) {
+        console.error(err)
+        res.status(500).redirect('/')
+    }
+})
+
+app.put('/markComplete', async (req, res) => {
+    try {
+        await db.collection('starwars').updateOne(
+            { thing: req.body.itemFromJS },
+            { $set: { completed: true } },
+            { sort: { _id: -1 }, upsert: false }
+        )
         console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
-
+        res.json('Marked Complete')
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err.message)
+    }
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
+app.put('/markUnComplete', async (req, res) => {
+    try {
+        await db.collection('starwars').updateOne(
+            { thing: req.body.itemFromJS },
+            { $set: { completed: false } },
+            { sort: { _id: -1 }, upsert: false }
+        )
+        console.log('Marked Uncomplete')
+        res.json('Marked Uncomplete')
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err.message)
+    }
+})
+
+app.delete('/deleteItem', async (req, res) => {
+    try {
+        await db.collection('starwars').deleteOne({ thing: req.body.itemFromJS })
         console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+        res.json('Todo Deleted')
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err.message)
+    }
 })
